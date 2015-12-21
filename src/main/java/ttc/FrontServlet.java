fd9027a0a324ee4fd01780f1b7286f8d35de2c24
blue-210 <j5hfca7pm@gmail.com>
2015-12-21 15:06:29 +0900

    変更取り込み

---
+++ src/main/java/ttc/FrontServlet.java
@@ -0,0 +1,44 @@
+package ttc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ttc.controller.ApplicationController;
+import ttc.controller.WebApplicationController;
+import ttc.context.RequestContext;
+import ttc.context.ResponseContext;
+
+import ttc.exception.PresentationException;
+
+public class FrontServlet extends HttpServlet{
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException{
+
+			req.setCharacterEncoding("UTF-8");
+
+			ApplicationController app = new WebApplicationController();
+
+			RequestContext reqc = app.getRequest(req);
+
+			ResponseContext resc = null;
+
+			try{
+				resc = app.handleRequest(reqc);
+				resc.setResponse(res);
+				app.handleResponse(reqc, resc);
+
+			}catch(PresentationException e){
+				throw new IOException(e.getMessage(),e);
+			}
+
+
+		}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException{
+			doPost(req,res);
+		}
+}
---
+++ src/main/java/ttc/controller/WebApplicationController.java
@@ -0,0 +1,57 @@
+package ttc.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+import ttc.context.RequestContext;
+import ttc.context.ResponseContext;
+import ttc.context.WebRequestContext;
+import ttc.context.WebResponseContext;
+import ttc.command.AbstractCommand;
+import ttc.util.CommandFactory;
+import ttc.exception.PresentationException;
+
+public class WebApplicationController implements ApplicationController{
+
+	public RequestContext getRequest(Object request){
+
+		RequestContext reqc = new WebRequestContext();
+
+		reqc.setRequest(request);
+
+		return reqc;
+	}
+
+	public ResponseContext handleRequest(RequestContext reqc)throws PresentationException{
+
+		AbstractCommand command = CommandFactory.getCommand(reqc);
+		command.init(reqc);
+
+		ResponseContext resc = command.execute( new WebResponseContext() );
+
+		return resc;
+	}
+
+	public void handleResponse(RequestContext reqc, ResponseContext resc)
+        throws PresentationException
+    {
+
+		HttpServletRequest req = (HttpServletRequest) reqc.getRequest();
+		HttpServletResponse res = (HttpServletResponse) resc.getResponse();
+
+		req.setAttribute("result",resc.getResult());
+
+		RequestDispatcher rd = req.getRequestDispatcher(resc.getTarget());
+
+		try{
+			rd.forward(req,res);
+		} catch(ServletException e){
+			throw new PresentationException(e.getMessage(), e);
+		} catch(IOException e){
+			throw new PresentationException(e.getMessage(), e);
+		}
+	}
+}
