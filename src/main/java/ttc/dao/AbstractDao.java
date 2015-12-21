7e421932219e723f623b627bc9ce073efda795d9
MasakiFukuda <masaki1014@hotmail.co.jp>
2015-12-21 12:15:59 +0900

    Dao内部のメソッドの引数をMap型に変更

--- src/main/java/ttc/dao/AbstractDao.java
+++ src/main/java/ttc/dao/AbstractDao.java
@@ -2,8 +2,9 @@ package ttc.dao;
 
 import ttc.bean.Bean;
 import java.util.List;
+import java.util.Map;
 
 public interface AbstractDao{
-    public List read(Bean bean);
-    public int write(Bean bean);
+    public List read(Map map);
+    public int write(Map map);
 }
