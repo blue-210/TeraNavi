8373f396e5900cf3e234f8ff47fd7c59ea551dfb
blue-210 <j5hfca7pm@gmail.com>
2015-12-21 14:32:57 +0900

    サンプルクラスの修正

--- src/test/java/ttc/sample/SampleDaoTest.java
+++ src/test/java/ttc/sample/SampleDaoTest.java
@@ -3,6 +3,9 @@ package ttc.sample;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
+import org.dbunit.database.*;
+import ttc.util.MySqlConnectionManager;
+
 
 public class SampleDaoTest {
     // DBに接続し、初期化としてTableをいったんキレイにして期待されるデータをセットする
@@ -11,22 +14,39 @@ public class SampleDaoTest {
         SampleUsersBean bean = new SampleUsersBean();
         bean.setId("12345678");
         bean.setName("ぶるー");
+
+        // DBのセットアップ
+        // データセットの取得
+        Connection conn = null;
+        IDatabaseConnection connection = null;
+
+        try{
+            IDataSet dataset = new XlsDataSetBuilder().build(this.getClass().getResourceAsStream("testdata.xlsx"));
+            // セットアップ
+            conn = MySqlConnectionManager.getInstance().getConnection();
+            connection =  new DatabaseConnection(conn);
+
+            DatabaseOperation.CLEAN_INSERT.execute(connection,dataset);
+        }finally{
+            if(connection != null){
+                connection.close();
+            }
+            if(conn != null){
+                conn.close();
+            }
+        }
     }
 
-    @Test
-    public void writeでユーザを登録できる(){
+    public void readでユーザを1人取得できる() throws Exception{
         // 初期化
-        SampleUserDao sut = new SampleUserDao();
-        sut.write(bean);
-    }
+        SampleDao sut = new SampleDao();
+        SampleUsersBean bean = new SampleUsersBean();
 
-    @Test
-    public void readでユーザを1人取得できる(){
+        // 実行
+        List<SampleUsersBean> actual = sut.read(bean);
 
-    }
-
-    @Test
-    public void readでユーザーリストを取得できる(){
-
+        // 検証
+        assertThat(actual.get(0).getId(),is("12345678"));
+        assertThat(actual.get(1).getName(),is("青木隼人"));
     }
 }
