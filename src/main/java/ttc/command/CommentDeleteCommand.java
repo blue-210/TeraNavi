ad18c6ab01d8c017cd38e560a9c9d37e33e5a221
MasakiFukuda <masaki1014@hotmail.co.jp>
2015-12-22 13:57:14 +0900

    コメントの削除用のコマンドの追加

--- src/main/java/ttc/command/CommentDeleteCommand.java
+++ src/main/java/ttc/command/CommentDeleteCommand.java
@@ -11,6 +11,35 @@ import ttc.exception.IntegrationException;
 import ttc.util.factory.AbstractDaoFactory;
 import ttc.dao.AbstractDao;
 
-public class CommentDeleteCommand implements AbstractCommand{
-    
+import java.util.Map;
+import java.util.HashMap;
+
+public class CommentDeleteCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc)throws BusinessLogicException{
+        try{
+            RequestContext reqc = getRequestContext();
+
+            String commentId = reqc.getParameter("commentId")[0];
+
+
+            Map params = new HashMap();
+            params.put("commentId",commentId);
+
+            MySqlConnectionManager.getInstance().beginTransaction();
+
+            AbstractDaoFactory factory = AbstractDaoFactory.getFactory("comment");
+            AbstractDao dao = factory.getAbstractDao();
+            dao.update(params);
+
+            MySqlConnectionManager.getInstance().commit();
+            MySqlConnectionManager.getInstance().closeConnection();
+
+
+            resc.setTarget("blogCreate");
+
+            return resc;
+        }catch(IntegrationException e){
+            throw new BusinessLogicException(e.getMessage(),e);
+        }
+    }
 }
