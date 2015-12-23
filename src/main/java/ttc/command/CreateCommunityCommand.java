e717c195491865e6bd1da689ad4442c50eccc378
MasakiFukuda <masaki1014@hotmail.co.jp>
2015-12-23 14:24:03 +0900

    コミュニティ作成機能の実装

---
+++ src/main/java/ttc/command/CreateCommunityCommand.java
@@ -0,0 +1,56 @@
+package ttc.command;
+
+import ttc.context.RequestContext;
+import ttc.context.ResponseContext;
+
+import ttc.util.MySqlConnectionManager;
+
+import ttc.exception.BusinessLogicException;
+import ttc.exception.IntegrationException;
+
+import ttc.util.factory.AbstractDaoFactory;
+import ttc.dao.AbstractDao;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class CreateCommunityCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc)throws BusinessLogicException{
+        try{
+            RequestContext reqc = getRequestContext();
+
+            HashMap params = new HashMap();
+
+            params.put("commName",reqc.getParameter("commName")[0]);
+            params.put("commProfile",reqc.getParameter("commProfile")[0]);
+
+            String icon = (reqc.getParameter("commIcon")[0]);
+            if(icon!=null && icon.length()!=0){
+                params.put("commIcon",icon);
+            }
+
+            String header = (String)reqc.getParameter("commHeader")[0];
+            if(header != null && header.length() != 0){
+                params.put("commHeader",header);
+            }
+
+            params.put("userName",reqc.getParameter("userName")[0]);
+
+            MySqlConnectionManager.getInstance().beginTransaction();
+
+            AbstractDaoFactory factory = AbstractDaoFactory.getFactory("Community");
+            AbstractDao dao = factory.getAbstractDao();
+            dao.update(params);
+
+            MySqlConnectionManager.getInstance().commit();
+            MySqlConnectionManager.getInstance().closeConnection();
+
+
+            resc.setTarget("communityCreateResult");
+
+            return resc;
+        }catch(IntegrationException e){
+            throw new BusinessLogicException(e.getMessage(),e);
+        }
+    }
+}
---
+++ src/main/java/ttc/dao/CommunitiesDao.java
@@ -0,0 +1,86 @@
+package ttc.dao;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import ttc.util.MySqlConnectionManager;
+import ttc.bean.Bean;
+import ttc.bean.CommunityBean;
+import ttc.exception.IntegrationException;
+
+public class CommunitiesDao implements AbstractDao{
+
+    public Bean read(Map map)throws IntegrationException{
+        return null;
+    }
+
+    public int update(Map map)throws IntegrationException{
+        return 0;
+    }
+
+    public int insert(Map map)throws IntegrationException{
+        PreparedStatement pst = null;
+        int result = 0;
+        try{
+            Connection cn = null;
+            cn = MySqlConnectionManager.getInstance().getConnection();
+            StringBuffer sql = new StringBuffer();
+            sql.append("insert into communities(");
+            sql.append("community_name,community_profile");
+            sql.append("community_icon_path,community_header_path");
+            sql.append("community_created_date,fk_user_name");
+            sql.append("community_delete_flag");
+            sql.append(" values(?,?,?,?,?,?,?)");
+
+            pst = cn.prepareStatement(new String(sql));
+
+            pst.setString(1,(String)map.get("commName"));
+            pst.setString(2,(String)map.get("commProfile"));
+
+            if(map.containsKey("commIcon")){
+                pst.setString(3,(String)map.get("commIcon"));
+            }else{
+                // デフォルト画像のパス
+                // プロパティファイルを使って書き換えられるようにするのもありかも
+                pst.setString(3,"/images/icon/icon.jpg");
+            }
+
+            if(map.containsKey("commHeader")){
+                pst.setString(4,(String)map.get(commHeader));
+            }else{
+                pst.setString(4,"/images/header/header.jpg");
+            }
+
+            pst.setString(5,"sysdate");
+            pst.setString(6,(String)map.get("userName"));
+            pst.setString(7,"0");
+
+            result = pst.executeUpdate();
+
+        }catch(SQLException e){
+            MySqlConnectionManager.getInstance().rollback();
+            throw new IntegrationException(e.getMessage(),e);
+        }finally{
+            try{
+                if(pst!=null){
+                    pst.close();
+                }
+            }catch(SQLException e){
+                throw new IntegrationException(e.getMessage(),e);
+            }
+        }
+
+        return result;
+
+    }
+
+    public List readAll(Map map)throws IntegrationException{
+        return null;
+    }
+
+}
