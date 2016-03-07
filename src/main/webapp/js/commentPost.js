54ce4ff1fad780f6d321d3b3d40c1cb780909410
TomokiNakashima <tn14304012@ga.tera-house.ac.jp>
2016-03-07 16:52:02 +0900

    コメント改行！

--- src/main/webapp/js/commentPost.js
+++ src/main/webapp/js/commentPost.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,9 +14,9 @@ $(function(){
 		url:'/TeraNavi/front/compost',
 		dataType:'json',
 		data:null
-		
+
 	};
-	
+
 });
 
 $(document).on("click","#commentSubmit",function(){
@@ -29,7 +29,7 @@ function sendComment(){
 		articleId:$("#primaryArticleId").val(),
 		body:$("#commentBody").val()
 	};
-	
+
 	ajaxSettings.success = function(data){
 		var cDiv = $("#commentPostDiv");
 		console.log(data.length);
@@ -43,14 +43,14 @@ function sendComment(){
 			var cDate = data[i].commentDate;
 			var body = data[i].commentBody;
 			var userId = data[i].userId;
-			
-			
-			
+
+
+
 			cDiv.append('<div class="row">\n\
 							<div class="col-md-1 col-md-offset-2">\n\
 								<img src="'+userIcon+'" class="img-responsive"><a href="#"><p class="text-center">'+userName+'</p></a>\n\
 							</div>\n\
-							<div class="col-md-7"><p>'+cDate+'</p><p>'+body+'</p>\n\
+							<div class="col-md-7"><p>'+cDate+'</p><pre>'+body+'</pre>\n\
 								<a href="#" class="dropdown pull-right dropdown-toggle" data-toggle="dropdown"><i class="-o -square fa fa-ellipsis-h fa-fw fa-lg text-muted"></i></a>\n\
 								<ul class="dropdown-menu dropdown-menu-right">\n\
 									<li><a onclick="commentCaution(\''+userId+'\')">このコメントを通報する</a></li>\n\
@@ -58,10 +58,10 @@ function sendComment(){
 								</ul>\n\
 							</div>\n\
 						</div>');
-			cDiv.append('<div class="row"><div class="col-md-8 col-md-offset-2"><hr></div></div>');			
+			cDiv.append('<div class="row"><div class="col-md-8 col-md-offset-2"><hr></div></div>');
 		}
 	};
 
 	ajax = $.ajax(ajaxSettings);
-	
+
 }
