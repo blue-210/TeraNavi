7badbdaefb007a89dbed635e8ca484b2795713d7
MasakiFukuda <masaki1014@hotmail.co.jp>
2016-03-08 16:21:18 +0900

    ファイルのアップロード時にファイル名をランダムでつける形に修正

--- src/main/java/ttc/FileUploadServlet.java
+++ src/main/java/ttc/FileUploadServlet.java
@@ -22,17 +22,19 @@ import java.util.List;
 import java.util.Iterator;
 import ttc.util.ImageResizer;
 
-public class FileUploadServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException{
-		doPost(req,res);
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class FileUploadServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		doPost(req, res);
 	}
 
-	public void doPost(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException{
-
-
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
 		req.setCharacterEncoding("utf-8");
 
+//		保存先のパス
 		String path = "/tmp";
 
 		String resultPath = null;
@@ -46,45 +48,60 @@ public class FileUploadServlet extends HttpServlet{
 
 		boolean resizeFlg = true;
 
-		if(info != null && info.equals("/header")){
+		if (info != null && info.equals("/header")) {
 			width = 1500;
 			height = 500;
-		}else if(info != null && info.equals("/article")){
+		} else if (info != null && info.equals("/article")) {
 			resizeFlg = false;
 			//リサイズしません
-		}else{
+		} else {
 			width = 400;
 			height = 400;
 		}
 
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		ServletFileUpload sfu = new ServletFileUpload(factory);
-		try{
+		try {
 			List list = sfu.parseRequest(req);
 			Iterator it = list.iterator();
-			while(it.hasNext()){
-				FileItem item = (FileItem)it.next();
+			while (it.hasNext()) {
+				FileItem item = (FileItem) it.next();
 
-				if(!item.isFormField()){
+				if (!item.isFormField()) {
 					String fileName = item.getName();
-					if((fileName!=null)&&(!fileName.equals(""))){
-						File target = new File(fileName);
-						fileName = target.getName();
+					if ((fileName != null) && (!fileName.equals(""))) {
+//						File target = new File(fileName);
+//						
+//						fileName = target.getName();
 
-						item.write(new File(path+"/"+fileName));
+//						ファイルの拡張子を保持する変数
+
+						String suffix = "";
+						int point = fileName.lastIndexOf(".");
+//						ファイル名から一番最後の.の位置を取得
+						
+						if (point != -1) {	//ファイル名に.が含まれているかどうかのチェック
+//							最後の.の次の文字から先を拡張子として切り取る
+							suffix = fileName.substring(point);
+						}
+
+						fileName = RandomStringUtils.randomAlphanumeric(10)+suffix;
+
+//						ファイルの書き込み
+						item.write(new File(path + "/" + fileName));
 
 						String result = null;
 
-						if(resizeFlg){
-								result = ImageResizer.doResize(new File(path+"/"+fileName), width, height,path+"/"+fileName);
-						}else{
+						if (resizeFlg) {
+							result = ImageResizer.doResize(new File(path + "/" + fileName), width, height, path + "/" + fileName);
+						} else {
 							String inputPath = fileName;
-					        String ext = inputPath.substring(inputPath.lastIndexOf(".") + 1);
+							String ext = inputPath.substring(inputPath.lastIndexOf(".") + 1);
 
-							String[] temp = fileName.split("\\.",0);
+							String[] temp = fileName.split("\\.", 0);
 							StringBuffer buff = new StringBuffer();
-							for(int i = 0;i < temp.length;i++){
-								if(i==temp.length-1){
+							for (int i = 0; i < temp.length; i++) {
+								if (i == temp.length - 1) {
 									buff.append("_resize.");
 								}
 								buff.append(temp[i]);
@@ -92,22 +109,20 @@ public class FileUploadServlet extends HttpServlet{
 							result = new String(buff);
 						}
 
-						resultPath = "http://"+hostName+"/TeraNavi/imgPath/"+result;
+//						戻り値としてサーバ上の画像のパスを返す
+						resultPath = "http://" + hostName + "/TeraNavi/imgPath/" + result;
 					}
 				}
 
 			}
 
-
-		}catch(FileUploadException e){
-			throw new IOException(e.getMessage(),e);
-		}catch(Exception e){
-			throw new IOException(e.getMessage(),e);
+		} catch (FileUploadException e) {
+			throw new IOException(e.getMessage(), e);
+		} catch (Exception e) {
+			throw new IOException(e.getMessage(), e);
 		}
 
-
-
-		String responseJson = "{\"result\":\""+resultPath+"\"}";
+		String responseJson = "{\"result\":\"" + resultPath + "\"}";
 		res.setContentType("application/json;charset=UTF-8");
 
 		PrintWriter writer = res.getWriter();
@@ -117,10 +132,10 @@ public class FileUploadServlet extends HttpServlet{
 		writer.close();
 	}
 
-	private String getFilename(Part part){
-		for(String cd : part.getHeader("Content-Disposition").split(";")){
-			if(cd.trim().startsWith("filename")){
-				return cd.substring(cd.indexOf('=') + 1).trim().replace("\"","");
+	private String getFilename(Part part) {
+		for (String cd : part.getHeader("Content-Disposition").split(";")) {
+			if (cd.trim().startsWith("filename")) {
+				return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
 			}
 		}
 
