46e4c0be74611f9650eb63829662ccecf352ddd9
MasakiFukuda <masaki1014@hotmail.co.jp>
2016-03-14 13:22:29 +0900

    記事編集画面で設定済みのタグはチェックをつけておく処理を実装

--- src/main/java/ttc/command/GetTagListCommand.java
+++ src/main/java/ttc/command/GetTagListCommand.java
@@ -1,5 +1,6 @@
 package ttc.command;
 
+import java.util.ArrayList;
 import ttc.context.RequestContext;
 import ttc.context.ResponseContext;
 
@@ -10,7 +11,9 @@ import ttc.exception.integration.IntegrationException;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import ttc.bean.TagBean;
 
 import ttc.util.factory.AbstractDaoFactory;
 import ttc.dao.AbstractDao;
@@ -21,6 +24,19 @@ public class GetTagListCommand extends AbstractCommand{
         try{
 
             RequestContext reqc = getRequestContext();
+			
+			String articleId = null;
+			boolean flag = false;
+			//記事に付いているタグを取得するかどうかの判定フラグ
+			
+			List result2 = new ArrayList();
+//			記事に付いているタグを取得する場合に結果として返す変数
+			try{
+				articleId = reqc.getParameter("articleId")[0];
+				flag = true;
+			}catch(NullPointerException e){
+//				パラメータが送られていない時に発生するぬるぽのためのキャッチ処理
+			}
 
             Map params = new HashMap();
             //すべてのタグ一覧をくださいというフラグ
@@ -31,11 +47,50 @@ public class GetTagListCommand extends AbstractCommand{
             AbstractDaoFactory factory = AbstractDaoFactory.getFactory("tag");
             AbstractDao dao = factory.getAbstractDao();
             List tags = dao.readAll(params);
+			
+			if(flag){
+				params.clear();
+				params.put("articleId",articleId);
+				List tagData = dao.readAll(params);
+//				記事に付いているタグを取得
+				
+				Iterator itr = tags.iterator();
+				while(itr.hasNext()){
+					TagBean tag = (TagBean)itr.next();
+					Map tagInfo = new HashMap();
+					
+					boolean checkFlag = false;
+					//タグが既にチェックされているかのフラグ
+					
+					Iterator itr2 = tagData.iterator();
+					while(itr2.hasNext()){
+						TagBean tagTemp = (TagBean)itr2.next();
+						if(tagTemp.getId().equals(tag.getId())){
+							checkFlag = true;
+						}
+					}
+					
+					if(checkFlag){
+//						タグが既に記事に付いているものだったらstatusパラメータを変える
+						tagInfo.put("status","true");
+					}else{
+						tagInfo.put("status", "false");
+						
+					}
+					tagInfo.put("tag", tag);
+					result2.add(tagInfo);
+				}
+			}
 
             MySqlConnectionManager.getInstance().commit();
             MySqlConnectionManager.getInstance().closeConnection();
 
-            resc.setResult(tags);
+			if(flag){
+				resc.setResult(result2);
+			}else{
+				resc.setResult(tags);
+				
+			}
 
             return resc;
 
