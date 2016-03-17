2ded355232d9cb4d025d7f214ba1eb979e5fb122
TsuchiyaTomoya <tunatuna7255@yahoo.co.jp>
2016-03-17 12:04:50 +0900

    header.jspをトークンに対応

--- src/main/java/ttc/filter/CsrfTokenFilter.java
+++ src/main/java/ttc/filter/CsrfTokenFilter.java
@@ -15,30 +15,32 @@ import javax.servlet.FilterChain;
 public class CsrfTokenFilter implements Filter{
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain){
     try{
-           
+
 
            // セッションが存在しない場合NULLを返す
            HttpSession session = ((HttpServletRequest)req).getSession();
 		   String token = null;
 		   String paramToken = null;
-		   
+
 		   try{
 				//パラメータからトークンを取得する際にトークンがないと
 				//ぬるぽが出る気がするのでtry-catchをつけてます。なくてもいいかも
 				token = (String)session.getAttribute("token");
 				paramToken = req.getParameter("token");
-		   }catch(NullPointerException e){
-			   //パラメータが無かった場合の処理
-			   RequestDispatcher dispatcher = req.getRequestDispatcher("/index");
-               dispatcher.forward(req,res);
-		   }
-		   
-           if(token.equals(paramToken)){
-               // session内のトークンとパラメータとして送られてきたトークンを比較
-			   //トークンが一致したときのみ正しい送り先へrequestを跳ばす
-               chain.doFilter(req, res);
-           }else{
-               
+
+                System.out.println("token="+token+"  paramToken="+paramToken);
+
+               if(token.equals(paramToken)){
+                   // session内のトークンとパラメータとして送られてきたトークンを比較
+    			   //トークンが一致したときのみ正しい送り先へrequestを跳ばす
+                   chain.doFilter(req, res);
+               }else{
+
+                   RequestDispatcher dispatcher = req.getRequestDispatcher("/index");
+                   dispatcher.forward(req,res);
+               }
+           }catch(NullPointerException e){
+               //パラメータが無かった場合の処理
                RequestDispatcher dispatcher = req.getRequestDispatcher("/index");
                dispatcher.forward(req,res);
            }
